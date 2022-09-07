64bc4ac31d49c64c97f8888fd3dda9d2c0d19a5c
Erik Kjosbakken <erik@skyetec.no>
2022-09-07 13:14:34 +0200

    Test info

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const getUser = async () => {
+  const response = await fetch('/.auth/me');
+  const payload = await response.json();
+  const { clientPrincipal } = payload;
+  return clientPrincipal;
+}
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    getUser().then(user => setUser(user));
+  }, [])
+
+  return (
+    <div>
+      Hello {user?.userDetails}<pre>{JSON.stringify(user)}</pre>
+    </div>
+  );
 }
 
 export default App;
